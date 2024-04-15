8f1a184968b5d92d1d3cb6635ef571c2294de881
SamruddhiKulkarni672 <97782085+SamruddhiKulkarni672@users.noreply.github.com>
2024-04-15 16:19:12 +0000

    practice

--- practice1/test.js
+++ practice1/test.js
@@ -1,1 +1,73 @@
-console.log("samruddhi")
+// // console.log("samruddhi")
+//  const accountID=12345
+//  let accountEmail="sam@1234"
+//  var accountPassword="123@4"
+//  accountCity="pune"
+// let accountState;
+
+// //  accountID=2 not allowed
+
+// accountEmail="arrtg"
+// accountPassword="1234"
+// accountCity="beed"
+
+// /*prefer not to use block beacuse of block scope and functional scope*/
+
+// console.table([accountEmail,accountID,accountPassword,accountCity,accountState] )
+
+//datatypes
+
+// "use strict";//treats all js code with newr version
+// // alert(3+3) we are using node js not browser
+// let num=3
+// let name='samruddhi'
+// let student=false
+
+//number
+//bigint
+//string-""
+//boolean - true/false
+//null - stand alone value
+//symbol- uniqueness
+
+
+//object
+
+// console.log(typeof null)//object
+// console.log(typeof undefined)//undefined
+
+// let score=null
+// console.log(typeof score)
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//'33'= 33
+//"33abd"= NaN
+// let logedIn=true
+//  let boolIslogedIn=Boolean(logedIn) 
+//  console.log(boolIslogedIn)
+//  console.log(typeof boolIslogedIn)
+
+// 1= true 0=false
+//""=false "ers"=true
+
+// **************************oprrations***************************//
+
+// let value=3
+// let negvalue= -value
+// console.log(negvalue )
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1 );
+// console.log(2<1);
+
+// console.log("02">1);  olp=true
+// console.log("2">1);   olp=true
+
+// === => strict check ,checks datatypes as well
+//console.log("2"===1); false
+
+
